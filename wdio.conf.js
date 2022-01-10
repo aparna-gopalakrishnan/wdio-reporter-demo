753c06359207bc5d8c3e4e8081ed7d387021550c
Aparna Gopalakrishnan <13162138+aparna-gopalakrishnan@users.noreply.github.com>
2022-01-10 13:57:17 +0000

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -116,8 +116,14 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
+    user: 'user.name',
+    key: 'key',
+    logFile: './logDir/api.log',
     services: [
         'chromedriver',
+        // ['lambdatest', {
+        //     tunnel: true
+        // }],
     // [TimelineService], //enable only when running using Timeline service
 ],
     
